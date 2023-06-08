7c6854648c12fbd573aab206be5854bf49e3a343
AndoniUTT <AndoniUTT>
2023-06-08 14:43:42 -0600

    03 operadores

---
+++ operadores.py
@@ -0,0 +1,18 @@
+#Operadores matematicos
+#+-*%
+#/Division flotante
+#//Division Entera
+#**Potencia
+print(10+2)
+print(10-2)
+print(10*2)
+print(10%2)
+print(10/2)
+print(10//2)
+print(10**2)
+
+#Operadores string
+# + para contatenar
+# * para replicar
+print("Hola mundo" + ". Es mi primer codigo")
+print(" Hola mundo " * 3)
