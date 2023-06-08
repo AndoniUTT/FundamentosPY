1d3cfcc1f4b3487cbe721cc934fbe55bcd700f24
AndoniUTT <AndoniUTT>
2023-06-08 14:32:54 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Python es NO tipado
+y = 36.3
+x = 2
+w = x + y 
+print(y)
+print(x)
+print(w)
+
+"""
+Hola soy Andoni, tengo 22 años y soltero = true
+"""
+#utilizando format string
+print("Hola mi nombre es {}, tengo {} y soltero={}".format(y,x,w))
+print(f"Hola mi nombre es {y}, tengo {x} y soltero={w}")
