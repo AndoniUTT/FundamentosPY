ff98813e44955e3fa8ccf7ab01e2187ea3d8f428
AndoniUTT <AndoniUTT>
2023-06-08 14:55:37 -0600

    04 entrada

---
+++ entrada.py
@@ -0,0 +1,9 @@
+name = input("Quien eres? ")
+print(f"Hola, bienvenido {name}")
+
+a=input("De donde eres? ")
+b=input("Cuantos años tienes? ")
+c=int(input("Escribe un numero: "))
+print(f"Eres de {a} y  tienes {b} años.")
+
+print(b+c)
