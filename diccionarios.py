e17200d5e3745f596a913ec8ff059ec77e064689
AndoniUTT <andoni2020lopez@gmail.com>
2023-06-15 13:39:49 -0600

    10 Diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,16 @@
+#Diccinarios
+#{"key": "value", "key": "value"team")
+
+team={
+    "name": "city",
+    "country": "England",
+    "champions": 1,
+    "players": ['Halland', 'Grenlish']
+}
+print(type(team))
+print(team)
+print(team["name"])
+print(team["players"])
+team["players"].append("Kevin")
+print(team)
+
